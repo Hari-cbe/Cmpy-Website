ced7e4e49dc893bb417e2be65927fb36be970380
Hari-cbe <harisachin1025@gmail.com>
2023-12-06 19:50:26 +0530

    Added Tailwind and made some changes to Public Fol

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container drop-shadow-sm">
+      <h3 className="text-3xl font-bold underline font-extralight hover:shadow-xl">
+        Confuss : "The beginning of a great journey begins with a single step"
+      </h3>
     </div>
   );
 }
